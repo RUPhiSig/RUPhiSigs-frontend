21dea7eb4c52fabd257e2fa58a4e086a9d77a05f
Neil Malu <Neil@Neils-MBP.home>
2016-12-14 00:10:40 -0500

    First commit with skeleton of the menubar

--- src/client.js
+++ src/client.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from './App';
-
+import MenuTabs from './menu';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 const Test = () => (<h1><Link to="/">Test1</Link></h1>);
 
 const Test0 = () => (<h1><Link to="/test">Test0.0</Link></h1>);
 
-injectTapEventPlugin();
 
-ReactDOM.render(
+/*ReactDOM.render(
   (
   <MuiThemeProvider>
     <Router history={browserHistory}>
@@ -23,4 +23,19 @@ ReactDOM.render(
   </MuiThemeProvider>
   ),
   document.getElementById('root')
+);*/
+
+ReactDOM.render(
+  (
+  <MuiThemeProvider>
+    <Router history={browserHistory}>
+      <Route path="/" component={MenuTabs}>
+        <IndexRoute component={Test0} />
+        <Route path="/test" component={Test} />
+      </Route>
+    </Router>
+  </MuiThemeProvider>
+  ),
+  document.getElementById('root')
 );
+
---
+++ src/menu.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import Slider from 'material-ui/Slider';
+
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
+
+// function handleActive(tab) {
+//   alert(`A tab with this route property ${tab.props['data-route']} was activated.`);
+// }
+
+export default class MenuTabs extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'a',
+    };
+  }
+
+  handleChange = (value) => {
+    this.setState({
+      value: value,
+    });
+  };
+
+  render() {
+    return (
+      <Tabs
+        value={this.state.value}
+        onChange={this.handleChange}
+      >
+        <Tab label="Home" value="a" >
+          <div>
+            <h2 style={styles.headline}>Controllable Tab A</h2>
+            <p>
+              Tabs are also controllable if you want to programmatically pass them their values.
+              This allows for more functionality in Tabs such as not
+              having any Tab selected or assigning them different values.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Members" value="b">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="About Us" value="c">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Contact Us" value="d">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+      </Tabs>
+    );
+  }
+}
